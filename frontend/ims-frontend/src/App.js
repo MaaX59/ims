5813c560e64ab5f87aa5c7075f015075d5c54f1e
MaaX59 <Maax.strandberg@gmail.com>
2023-10-30 19:06:59 +0100

    more set up

--- frontend/ims-frontend/src/App.js
+++ frontend/ims-frontend/src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
+import "./App.css";
+import { Navbar, ViewProject, NewProject, AddItem } from "./Container/index";
 
 function App() {
-  return (
-    <div className="App">
-      adadw
-    </div>
-  );
+  return <div className="App">
+    <AddItem />
+  </div>;
 }
 
 export default App;
---
+++ frontend/ims-frontend/src/Container/index.js
@@ -0,0 +1,9 @@
+import ViewProject from './ViewProject/ViewProject';
+import NewProject from './NewProject/NewProject';
+import Navbar from './Navbar/Navbar';
+import AddItem from'./AddItem/AddItem';
+
+
+export {
+    Navbar, ViewProject, NewProject, AddItem
+};
