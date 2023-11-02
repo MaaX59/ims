5845b94ad39b217a193f9ff0f62346fbc4a0185c
MaaX59 <maax.strandberg@gmail.com>
2023-11-02 12:14:36 +0100

    install cors

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mysql = require('mysql')
+const mysql = require("mysql");
+const cors = require("cors")
+
+app.use(cors());
 
 const db = mysql.createConnection({
-user:'root',
-host:'localhost',
-password:'password',
-database:'IMS'
-})
+  user: "root",
+  host: "localhost",
+  password: "password",
+  database: "IMS",
+});
 
+app.listen(3001, () => {
+  console.log("server running at port 3001");
+});
 
-app.listen(3001, ()=> {
-    console.log("server running at port 3001")
-})
+app.post("/create", (req, res) => {
+  const project_name = req.body.project_name;
+  const project_description = req.body.project_description;
+  console.log(project_name);
 
-app.post('/create', (req, res)=>{
-    const name = req.body.project_name;
-    const description = req.body.project_description;
-
-    db.query(
-        "INSERT INTO project(name, description) VALUES(?,?)", 
-        [name, description], (err, result)=>{
-            if(err){
-                console.log(err)
-            } else{
-                res.send("values sent to db")
-            }
-        }
-        )
-
-})
+  db.query(
+    "INSERT INTO project(name, description) VALUES(?,?)",
+    [project_name, project_description],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send("values sent to db");
+      }
+    }
+  );
+});
