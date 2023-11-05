9b2ce3d4d9f57ea3d8b2b97e1c800538de2334fc
MaaX59 <maax.strandberg@gmail.com>
2023-11-05 12:01:52 +0100

    frontend for getprojects from db done

--- frontend/ims-frontend/src/Container/ViewProject/ViewProject.jsx
+++ frontend/ims-frontend/src/Container/ViewProject/ViewProject.jsx
@@ -1,16 +1,40 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import "./ViewProject.css";
 import Navbar from "../Navbar/Navbar";
+import axios from "axios";
+import { server } from "../../server";
 
 const ViewProject = () => {
+
+  useEffect(() => {
+    getProjects();
+  }, []);
+
+  const [projectList, setProjectList] = useState([]);
+
+  const getProjects = async () => {
+    try{
+       setProjectList(await axios.get(`${server}/getprojectlist`)) 
+  
+
+    }catch (error){
+      console.log(`error fetching projects`, error)
+    }}
+ 
+
+
   return (
     <div className="app__viewproject">
       <Navbar />
-      <div className="app__viewproject-content">
+      <div className="app__viewproject-title">
         <h1>View projects</h1>
+      </div>
+      <div className="app__viewproject-projects">
+
+
       </div>
     </div>
   );
-};
+}
 
 export default ViewProject;
