88217276b9eb30af1f80413f36c638e879ad300f
MaaX59 <maax.strandberg@gmail.com>
2023-12-22 21:28:23 +0100

    start finished

--- frontend/ims-frontend/src/Container/Start/Start.jsx
+++ frontend/ims-frontend/src/Container/Start/Start.jsx
@@ -6,13 +6,13 @@ const Start = () => {
   return (
     <div className="app__start">
       <Link to="/viewproject">
-        <div className="app__start-private">
+        <div className="app__start-private" style={{ textDecoration: "none" }}>
           <h1>Private</h1>
           <h2>No Login Required</h2>
         </div>
       </Link>
       <Link to="/login">
-        <div className="app__start-company">
+        <div className="app__start-company" style={{ textDecoration: "none" }}>
           <h1>Company</h1>
           <h2>Login Required</h2>
         </div>
