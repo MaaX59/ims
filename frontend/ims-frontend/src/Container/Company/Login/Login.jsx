7727da6690c36cd545f022331739556bb6578445
MaaX59 <maax.strandberg@gmail.com>
2023-12-22 21:43:29 +0100

    login started

--- frontend/ims-frontend/src/Container/Company/Login/Login.jsx
+++ frontend/ims-frontend/src/Container/Company/Login/Login.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { React, useState } from "react";
+import axios from "axios";
+import { server } from "../../../server";
 import "./Login.css";
 
 const Login = () => {
@@ -7,6 +9,43 @@ const Login = () => {
       <div className="app__login-content">
         <div className="app__login-content-title">
           <h1>Login</h1>
+        </div>
+
+        <div className="input-container ic1">
+          <input
+            id="email"
+            className="input"
+            type="email"
+            placeholder=" "
+            onChange={(event) => {
+              // setProject_name(event.target.value);
+            }}
+            required
+          />
+
+          <label for="email" className="placeholder">
+            Email
+          </label>
+        </div>
+        <div className="input-container ic2">
+          <input
+            id="project-description"
+            className="input"
+            type="password"
+            placeholder=" "
+            onChange={(event) => {
+              // setProject_description(event.target.value);
+            }}
+          />
+          <label for="password" className="placeholder">
+            Password
+          </label>
+          <button type="submit" className="submit">
+            Login
+          </button>
+          <button type="text" className="submit">
+            Create User
+          </button>
         </div>
       </div>
     </div>
