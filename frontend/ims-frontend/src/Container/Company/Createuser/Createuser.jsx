8d739702a1a063ccb892d3c34b71a173736caaaf
MaaX59 <Maax.strandberg@gmail.com>
2023-12-25 22:27:12 +0100

    form visually works

--- frontend/ims-frontend/src/Container/Company/Createuser/Createuser.jsx
+++ frontend/ims-frontend/src/Container/Company/Createuser/Createuser.jsx
@@ -10,39 +10,109 @@ const Createuser = () => {
             {" "}
             <h1>Create User</h1>
           </div>
-          <div className="input-container ic1">
-            <input
-              id="email"
-              className="input"
-              type="email"
-              placeholder=" "
-              onChange={(event) => {
-                // setProject_name(event.target.value);
-              }}
-              required
-            />
+          <div className="app_createuser-form-firstrow">
+            <div className="input-container ic1">
+              <input
+                id="firstname"
+                className="input"
+                type="text"
+                placeholder=" "
+                onChange={(event) => {
+                  // setProject_name(event.target.value);
+                }}
+                required
+              />
 
-            <label for="email" className="placeholder">
-              Email
-            </label>
+              <label for="firstname" className="placeholder">
+                First Name*
+              </label>
+            </div>
+
+            <div className="input-container ic1">
+              <input
+                id="lastname"
+                className="input"
+                type="text"
+                placeholder=" "
+                onChange={(event) => {
+                  // setProject_description(event.target.value);
+                }}
+              />
+              <label for="lastname" className="placeholder">
+                Last Name*
+              </label>
+            </div>
           </div>
-          <div className="input-container ic2">
-            <input
-              id="project-description"
-              className="input"
-              type="password"
-              placeholder=" "
-              onChange={(event) => {
-                // setProject_description(event.target.value);
-              }}
-            />
-            <label for="password" className="placeholder">
-              Password
-            </label>
+
+          <div className="app_createuser-form-secondrow">
+            <div className="input-container ic1">
+              <input
+                id="email"
+                className="input"
+                type="email"
+                placeholder=" "
+                onChange={(event) => {
+                  // setProject_name(event.target.value);
+                }}
+                required
+              />
+
+              <label for="email" className="placeholder">
+                Email*
+              </label>
+            </div>
+            <div className="input-container ic1">
+              <input
+                id="project-description"
+                className="input"
+                type="password"
+                placeholder=" "
+                onChange={(event) => {
+                  // setProject_description(event.target.value);
+                }}
+              />
+              <label for="password" className="placeholder">
+                Password*
+              </label>
+            </div>
+          </div>
+
+          <div className="app_createuser-form-thirdrow">
+            <div className="input-container ic1">
+              <input
+                id="company_id"
+                className="input"
+                type="number"
+                placeholder=" "
+                onChange={(event) => {
+                  // setProject_name(event.target.value);
+                }}
+                required
+              />
+
+              <label for="company_id" className="placeholder">
+                Company ID
+              </label>
+            </div>
+
+            <div className="input-container ic1">
+              <input
+                id="company_password"
+                className="input"
+                type="password"
+                placeholder=" "
+                onChange={(event) => {
+                  // setProject_description(event.target.value);
+                }}
+              />
+              <label for="company_password" className="placeholder">
+                Company Password
+              </label>
+            </div>
           </div>
 
           <button type="submit" className="submit">
-            Login
+            Create
           </button>
         </div>
       </div>{" "}
