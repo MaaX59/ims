d1cecd9fc70b2718469cd5f5e392030ecdca4597
MaaX59 <maax.strandberg@gmail.com>
2024-01-12 14:16:34 +0100

    createuser continued

--- frontend/ims-frontend/src/Container/Company/Createuser/Createuser.jsx
+++ frontend/ims-frontend/src/Container/Company/Createuser/Createuser.jsx
@@ -5,10 +5,22 @@ const Createuser = () => {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
-  const [companyid, setCompanyid] = useState(null);
-  const [companyPassword, setCompanyPassword] = useState(null);
-  const [password, setPassword] = useState(null);
-  const [confirmedPassword, setConfirmedPassword] = useState(null);
+  const [companyid, setCompanyid] = useState("");
+  const [companyPassword, setCompanyPassword] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmedPassword, setConfirmedPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(firstName, password);
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setCompanyid("");
+    setCompanyPassword("");
+    setPassword("");
+    setConfirmedPassword("");
+  };
 
   return (
     <div className="app__createuser">
@@ -18,58 +30,59 @@ const Createuser = () => {
           <h1>Create User</h1>
         </div>
         <div className="app__createuser-form">
-          <div className="app_createuser-form-firstrow">
-            <div className="input-container ic1">
-              <input
-                id="firstname"
-                className="input"
-                type="text"
-                placeholder=" "
-                onChange={(event) => {
-                  setFirstName(event.target.value);
-                }}
-                required
-              />
+          <form onSubmit={handleSubmit}>
+            <div className="app_createuser-form-firstrow">
+              <div className="input-container ic1">
+                <input
+                  id="firstname"
+                  className="input"
+                  type="text"
+                  placeholder=" "
+                  onChange={(event) => {
+                    setFirstName(event.target.value);
+                  }}
+                  required
+                />
 
-              <label for="firstname" className="placeholder">
-                First Name*
-              </label>
+                <label for="firstname" className="placeholder">
+                  First Name*
+                </label>
+              </div>
+
+              <div className="input-container ic1">
+                <input
+                  id="lastname"
+                  className="input"
+                  type="text"
+                  placeholder=" "
+                  onChange={(event) => {
+                    setLastName(event.target.value);
+                  }}
+                />
+                <label for="lastname" className="placeholder">
+                  Last Name*
+                </label>
+              </div>
             </div>
 
-            <div className="input-container ic1">
-              <input
-                id="lastname"
-                className="input"
-                type="text"
-                placeholder=" "
-                onChange={(event) => {
-                  setLastName(event.target.value);
-                }}
-              />
-              <label for="lastname" className="placeholder">
-                Last Name*
-              </label>
-            </div>
-          </div>
+            <div className="app_createuser-form-secondrow">
+              <div className="input-container ic1">
+                <input
+                  id="email"
+                  className="input"
+                  type="email"
+                  placeholder=" "
+                  onChange={(event) => {
+                    setEmail(event.target.value);
+                  }}
+                  required
+                />
 
-          <div className="app_createuser-form-secondrow">
-            <div className="input-container ic1">
-              <input
-                id="email"
-                className="input"
-                type="email"
-                placeholder=" "
-                onChange={(event) => {
-                  setEmail(event.target.value);
-                }}
-                required
-              />
-
-              <label for="email" className="placeholder">
-                Email*
-              </label>
-            </div>
-            {/* <div className="input-container ic1">
+                <label for="email" className="placeholder">
+                  Email*
+                </label>
+              </div>
+              {/* <div className="input-container ic1">
               <input
                 id="project-description"
                 className="input"
@@ -83,79 +96,80 @@ const Createuser = () => {
                 Password*
               </label>
             </div> */}
-          </div>
-
-          <div className="app_createuser-form-thirdrow">
-            <div className="input-container ic1">
-              <input
-                id="company_id"
-                className="input"
-                type="number"
-                placeholder=" "
-                onChange={(event) => {
-                  setCompanyid(event.target.value);
-                }}
-              />
-
-              <label for="company_id" className="placeholder">
-                Company ID
-              </label>
             </div>
 
-            <div className="input-container ic1">
-              <input
-                id="company_password"
-                className="input"
-                type="password"
-                placeholder=" "
-                onChange={(event) => {
-                  setCompanyPassword(event.target.value);
-                }}
-              />
-              <label for="company_password" className="placeholder">
-                Company Password
-              </label>
+            <div className="app_createuser-form-thirdrow">
+              <div className="input-container ic1">
+                <input
+                  id="company_id"
+                  className="input"
+                  type="number"
+                  placeholder=" "
+                  onChange={(event) => {
+                    setCompanyid(event.target.value);
+                  }}
+                />
+
+                <label for="company_id" className="placeholder">
+                  Company ID
+                </label>
+              </div>
+
+              <div className="input-container ic1">
+                <input
+                  id="company_password"
+                  className="input"
+                  type="password"
+                  placeholder=" "
+                  onChange={(event) => {
+                    setCompanyPassword(event.target.value);
+                  }}
+                />
+                <label for="company_password" className="placeholder">
+                  Company Password
+                </label>
+              </div>
             </div>
-          </div>
 
-          <div className="app_createuser-form-fourthrow">
-            <div className="input-container ic1">
-              <input
-                id="password"
-                className="input"
-                type="password"
-                placeholder=" "
-                onChange={(event) => {
-                  setPassword(event.target.value);
-                }}
-                required
-              />
+            <div className="app_createuser-form-fourthrow">
+              <div className="input-container ic1">
+                <input
+                  id="password"
+                  className="input"
+                  type="password"
+                  placeholder=" "
+                  onChange={(event) => {
+                    setPassword(event.target.value);
+                  }}
+                  required
+                />
 
-              <label for="password" className="placeholder">
-                Personal Password*
-              </label>
+                <label for="password" className="placeholder">
+                  Personal Password*
+                </label>
+              </div>
+
+              <div className="input-container ic1">
+                <input
+                  id="confirm_password"
+                  className="input"
+                  type="password"
+                  placeholder=" "
+                  onChange={(event) => {
+                    setConfirmedPassword(event.target.value);
+                  }}
+                />
+                <label for="confirm_password" className="placeholder">
+                  Confirm Personal Password*
+                </label>
+              </div>
             </div>
+            <div className="form-required">* field is required</div>
 
-            <div className="input-container ic1">
-              <input
-                id="confirm_password"
-                className="input"
-                type="password"
-                placeholder=" "
-                onChange={(event) => {
-                  setConfirmedPassword(event.target.value);
-                }}
-              />
-              <label for="confirm_password" className="placeholder">
-                Confirm Personal Password*
-              </label>
-            </div>
-          </div>
-          <div className="form-required">* field is required</div>
-
-          <button type="submit" className="submit">
-            Create
-          </button>
+            <button type="submit" className="submit">
+              Create
+            </button>
+          </form>
         </div>
       </div>{" "}
     </div>
