e669eefa305c49a4bff79ffaa0eb3ac4232c1a09
MaaX59 <maax.strandberg@gmail.com>
2024-01-24 18:09:07 +0100

    update company item frontend finished and changes some styling

--- frontend/ims-frontend/src/Components/Company/UpdateCompanyItemForm/UpdateCompanyItemForm.jsx
+++ frontend/ims-frontend/src/Components/Company/UpdateCompanyItemForm/UpdateCompanyItemForm.jsx
@@ -1,5 +1,4 @@
 import { React, useState, useEffect } from "react";
-import "./UpdateItemForm.css";
 import axios from "axios";
 import { server } from "../../../server";
 
@@ -13,18 +12,28 @@ const UpdateCompanyItemForm = (props) => {
     setItem_amount(itemsToUpdate.item_amount);
     setProject_id(itemsToUpdate.project_id);
     setItem_id(itemsToUpdate.id);
-    setInStock(itemsToUpdate.inStock);
+    ifInStock(itemsToUpdate.in_stock);
     setPurchased_from(itemsToUpdate.purchased_from);
     setPurchased_price(itemsToUpdate.purchased_price);
     setNotes(itemsToUpdate.notes);
   }, []);
+
+  const ifInStock = (prop) => {
+    console.log(prop);
+    if (prop === 1) {
+      setInStock(true);
+    } else {
+      setInStock(false);
+    }
+  };
+
   const [item_id, setItem_id] = useState();
   const [item_name, setItem_name] = useState("");
   const [item_description, setItem_description] = useState("");
   const [item_location, setItem_location] = useState("");
   const [purchased_from, setPurchased_from] = useState("");
   const [purchased_price, setPurchased_price] = useState();
-  const [inStock, setInStock] = useState(false);
+  const [in_stock, setInStock] = useState();
   const [notes, setNotes] = useState("");
   const [item_amount, setItem_amount] = useState();
   const [project_id, setProject_id] = useState();
@@ -38,7 +47,7 @@ const UpdateCompanyItemForm = (props) => {
       item_amount,
       project_id,
       notes,
-      inStock,
+      in_stock,
       purchased_price,
       purchased_from
     );
@@ -49,91 +58,176 @@ const UpdateCompanyItemForm = (props) => {
         item_location: item_location,
         item_amount: item_amount,
         project_id: project_id,
+        notes: notes,
+        in_stock: in_stock,
+        purchased_price: purchased_price,
+        purchased_from: purchased_from,
       })
       .then(() => {
         props.getItems();
         props.setUpdate(false);
       });
 
+    setItem_id("");
     setItem_name("");
     setItem_description("");
     setItem_location("");
-    setItem_amount("");
+    setPurchased_from("");
+    setPurchased_price(null);
+    setInStock(false);
+    setNotes("");
+    setItem_amount(null);
+    setProject_id(null);
   };
 
   return (
-    <div className="form">
-      <div className="title">Update an item!</div>
-      <div className="input-container ic1">
-        <input
-          id="item-name"
-          className="input"
-          type="text"
-          value={item_name}
-          placeholder={itemsToUpdate ? itemsToUpdate.item_name : null}
-          onChange={(event) => {
-            setItem_name(event.target.value);
-          }}
-          required
-        />
+    <div className="app__add-company_item_form">
+      <div className="title">Update item</div>
+      <div className="form-2inRow">
+        <div className="input-container ic1">
+          <input
+            id="item-name"
+            className="input"
+            type="text"
+            value={item_name}
+            placeholder=" "
+            onChange={(event) => {
+              setItem_name(event.target.value);
+            }}
+            required
+          />
 
-        <label for="item-name" className="update-placeholder">
-          Item Name*
-        </label>
+          <label for="item-name" className="placeholder">
+            Item Name*
+          </label>
+        </div>
+        <div className="input-container ic1">
+          <input
+            id="item-amount"
+            className="input"
+            type="number"
+            value={item_amount}
+            placeholder=" "
+            min="1"
+            onChange={(event) => {
+              setItem_amount(event.target.value);
+            }}
+            required
+          />
+          <label for="item-amount" className="placeholder">
+            Amount*
+          </label>
+        </div>
       </div>
+
+      <div className="form-2inRow">
+        <div className="input-container ic1">
+          <input
+            id="purchased_from"
+            className="input"
+            type="text"
+            value={purchased_from}
+            placeholder=" "
+            onChange={(event) => {
+              setPurchased_from(event.target.value);
+            }}
+          />
+
+          <label for="purchased_from" className="placeholder">
+            Vendor item was purchased from
+          </label>
+        </div>
+
+        <div className="input-container ic1">
+          <input
+            id="purchased_price"
+            className="input"
+            type="number"
+            value={purchased_price}
+            placeholder=" "
+            min="1"
+            onChange={(event) => {
+              setPurchased_price(event.target.value);
+            }}
+          />
+          <label for="purchased_price" className="placeholder">
+            Purchase price
+          </label>
+        </div>
+      </div>
+
       <div className="input-container ic2">
         <input
           id="item-description"
           className="input"
           type="text"
           value={item_description}
-          placeholder={itemsToUpdate ? itemsToUpdate.item_description : null}
+          placeholder=" "
           onChange={(event) => {
             setItem_description(event.target.value);
           }}
         />
 
-        <label for="item-description" className="update-placeholder">
+        <label for="item-description" className="placeholder">
           Item description
         </label>
       </div>
 
-      <div className="input-container ic2">
-        <input
-          id="item-location"
-          className="input"
-          type="text"
-          value={item_location}
-          placeholder={itemsToUpdate ? itemsToUpdate.item_location : null}
-          onChange={(event) => {
-            setItem_location(event.target.value);
-          }}
-        />
-        <label for="item-location" className="update-placeholder">
-          Item Location
-        </label>
+      <div className="form-2inRow">
+        <div className="checkbox">
+          <input
+            id="inStock"
+            className="input-checkbox"
+            type="checkbox"
+            value={in_stock}
+            defaultChecked={in_stock}
+            onChange={(event) => {
+              setInStock(!in_stock);
+            }}
+          />
+          <label>In stock?</label>
+        </div>
+
+        <div
+          className="input-container ic1"
+          style={{ visibility: !in_stock ? "hidden" : "visible" }}
+        >
+          <input
+            id="item-location"
+            className="input"
+            type="text"
+            value={item_location}
+            placeholder=" "
+            onChange={(event) => {
+              setItem_location(event.target.value);
+            }}
+          />
+          <label for="item-location" className="placeholder">
+            Item Location
+          </label>
+        </div>
       </div>
 
       <div className="input-container ic2">
         <input
-          id="item-amount"
+          id="notes"
           className="input"
-          type="number"
-          value={item_amount}
-          placeholder={itemsToUpdate ? itemsToUpdate.item_amount : null}
-          min="1"
+          type="text"
+          value={notes}
+          placeholder=" "
           onChange={(event) => {
-            setItem_amount(event.target.value);
+            setNotes(event.target.value);
           }}
-          required
         />
-        <label for="item-amount" className="update-placeholder">
-          Amount*
+
+        <label for="notes" className="placeholder">
+          Notes
         </label>
       </div>
-      <div className="form-required"> * field is required</div>
+
+      <div className="company-form-required"> * field is required</div>
       <button type="text" className="submit" onClick={updateItem}>
-        Update
+        Create
       </button>
     </div>
   );
