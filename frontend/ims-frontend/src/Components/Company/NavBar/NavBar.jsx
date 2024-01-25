34bedc47bcb8289778ec6391c831028031e56ef0
MaaX59 <maax.strandberg@gmail.com>
2024-01-25 18:38:44 +0100

    navbar has links

--- frontend/ims-frontend/src/Components/Company/NavBar/NavBar.jsx
+++ frontend/ims-frontend/src/Components/Company/NavBar/NavBar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./NavBar.css";
+import * as Icon from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
@@ -8,7 +10,51 @@ const NavBar = () => {
         <span>IMS</span>
       </div>
       <div className="app__navbar_options">
-        Dashboard Inventory Purchases Sales Customers Log Manage IMS
+        <Link to="/dashboard" className="link">
+          <div className="app__navbar_options-single-dash">
+            {" "}
+            <Icon.FaChartBar /> <span>Dashboard</span>
+          </div>
+        </Link>
+
+        <Link to="/inventory" className="link">
+          <div className="app__navbar_options-single-inv">
+            <Icon.FaBox /> <span>Inventory</span>
+          </div>
+        </Link>
+
+        <Link to="/sales" className="link">
+          <div className="app__navbar_options-single-sales">
+            <Icon.FaAngleDoubleUp /> <span>Sales</span>
+          </div>
+        </Link>
+
+        <Link to="/purchases" className="link">
+          <div className="app__navbar_options-single-purch">
+            <Icon.FaAngleDoubleDown /> <span>Purchases</span>
+          </div>
+        </Link>
+
+        <Link to="/customers" className="link">
+          <div className="app__navbar_options-single-cust">
+            <Icon.FaPeopleArrows /> <span>Customers</span>
+          </div>
+        </Link>
+
+        <Link to="/log" className="link">
+          {" "}
+          <div className="app__navbar_options-single-log">
+            <Icon.FaList /> <span>Log</span>
+          </div>
+        </Link>
+
+        <Link to="/manage_ims" className="link">
+          <div className="app__navbar_options-single-ims">
+            {" "}
+            <Icon.FaDatabase />
+            <span>Manage IMS</span>
+          </div>
+        </Link>
       </div>
     </div>
   );
