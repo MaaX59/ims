9b793ed8138bb021383fc98297e87cb5951c8db3
MaaX59 <maax.strandberg@gmail.com>
2024-02-01 19:42:00 +0100

    working on sales

--- frontend/ims-frontend/src/Container/Company/SellSingleItem/SellSingleItem.jsx
+++ frontend/ims-frontend/src/Container/Company/SellSingleItem/SellSingleItem.jsx
@@ -25,6 +25,58 @@ const SellSingleItem = () => {
         <span>Item name {item && item.item_name} </span>
         <span>Item amount {item && item.item_amount}</span>
         <span>Item purchased price {item && item.purchased_price}</span>
+
+        <div className="form ">
+          <div className="title">Sell item</div>
+          <div className="input-container-company ic1">
+            <input
+              id="buyer_name"
+              className="input"
+              type="text"
+              placeholder=" "
+              onChange={(event) => {
+                // setProject_name(event.target.value);
+              }}
+              required
+            />
+
+            <label for="buyer_name" className="placeholder">
+              Name of buyer*
+            </label>
+          </div>
+          <div className="input-container-company ic2">
+            <input
+              id="sell_price"
+              className="input"
+              type="text"
+              placeholder=" "
+              onChange={(event) => {
+                // setSell_price(event.target.value);
+              }}
+              required
+            />
+
+            <label for="sell_price" className="placeholder">
+              Sell price per item*
+            </label>
+          </div>
+          <div className="input-container-company ic2">
+            <input
+              id="sell_amount"
+              className="input"
+              type="number"
+              placeholder=" "
+              onChange={(event) => {
+                // setSell_amount(event.target.value);
+              }}
+              required
+            />
+
+            <label for="sell_amount" className="placeholder">
+              Amount of items sold*
+            </label>
+          </div>
+        </div>
       </div>
     </div>
   );
